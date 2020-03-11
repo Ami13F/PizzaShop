046af3e22033bf4d2b0379ae6ea435f7b5fad597
Alex Dragan <adragan029@gmail.com>
2020-03-11 16:50:07 +0200

    [test] Add ECP test cases
    - implementation for ECP test cases

--- src/test/java/pizzashop/repository/PaymentRepositoryTest.java
+++ src/test/java/pizzashop/repository/PaymentRepositoryTest.java
@@ -1,5 +1,6 @@
 package pizzashop.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pizzashop.model.Payment;
@@ -15,12 +16,56 @@ class PaymentRepositoryTest {
     }
 
     @Test
-    void addPay() {
+    void addPay_TC1_ECP() {
+        final Payment payment = new Payment(1, PaymentType.CARD,13.97);
+        Assertions.assertTrue(paymentRepository.getAll().isEmpty());
+        paymentRepository.addPay(payment);
+        Assertions.assertFalse(paymentRepository.getAll().isEmpty());
+    }
 
+    @Test
+    void addPay_TC3_ECP() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            final Payment payment = new Payment(7,
+                    PaymentType.valueOf("BONURI"),12.0);
+            paymentRepository.addPay(payment);
+        });
+    }
+
+    @Test
+    void addPay_TC5_ECP() {
+        final Payment payment = new Payment(2, PaymentType.CASH, -1.0);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            paymentRepository.addPay(payment);
+        });
+    }
+
+    @Test
+    void addPay_TC6_ECP() {
+        final Payment payment = new Payment(0, PaymentType.CASH, 31.0);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            paymentRepository.addPay(payment);
+        });
+    }
+
+    @Test
+    void addPay_TC7_ECP() {
+        final Payment payment = new Payment(9, PaymentType.CASH, 12.3);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            paymentRepository.addPay(payment);
+        });
+    }
+
+    @Test
+    void addPay_TC1_BVA() {
+        final Payment payment = new Payment(8,	PaymentType.CARD, 43.15);
+        final int countNum = paymentRepository.getAll().size();
+        paymentRepository.addPay(payment);
+        Assertions.assertEquals(paymentRepository.getAll().size() - 1, countNum);
     }
 
     @Test
     void addPay_TC2_BVA(){
-        Payment payment = new Payment(7, PaymentType.CASH,43.20);
+        final Payment payment = new Payment(7, PaymentType.CASH,43.20);
     }
 }
